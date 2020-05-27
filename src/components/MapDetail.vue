8a2c458a11a513ced1b86d8df4f0f4b216013cc0
mskim <mskim1@wemb.co.kr>
2020-05-27 16:50:40 +0900

    상세 팝업 관련 기능 추가
    1. 사용 가능 유무 보여주기
    2. 신고하기 버튼 추가
    3. 네이버/다음 지도로 이동 버튼 추가

--- src/components/MapDetail.vue
+++ src/components/MapDetail.vue
@@ -1,38 +1,75 @@
 <template>
-    <el-dialog :title="S_NAME" :visible.sync="dialogVisible" width="560px" :class="$mq">
-        <p class="address">{{ ADDRESS }}</p>
-        <p class="container"><span class="s-group">{{ S_GROUP }}</span><span class="phone">{{ PHONE }}</span></p>
+    <el-dialog :title="mapData.S_NAME" :visible.sync="dialogVisible" width="560px" :class="$mq">
+        <div class="address">{{ mapData.ADDRESS }}</div>
+        <div class="use-yn">
+                {{ checkUseYN }}
+        </div>
+        <div class="container">
+            <div class="detail">
+                <span class="s-group">{{ mapData.S_GROUP }}</span><span class="phone">{{ mapData.PHONE }}</span>
+            </div>
+            <div class="map-detail">
+                <i class="el-icon-edit" @click="linkNaver"></i>
+                <i class="el-icon-share" @click="linkKakao"></i>
+                <i class="el-icon-delete" @click="report"></i>
+            </div>
+        </div>
     </el-dialog>
 </template>
 
 <script>
 import { EventBus } from '../EventBus';
+import { reportAddress } from '../api/addressAPI';
 
 export default {
     data() {
         return {
             dialogVisible: false,
-            S_GROUP: '',
-            ADDRESS: '',
-            S_NAME: '',
-            PHONE: ''
+            mapData : {}
         }
     },
     created() {
         const self = this;
-        EventBus.$on('open', ({ S_GROUP = '', ADDRESS = '', S_NAME = '', PHONE = '' }) => {
+        EventBus.$on('open', (mapData) => {
             self.dialogVisible = !self.dialogVisible;
-            self.S_GROUP = S_GROUP;
-            self.ADDRESS = ADDRESS;
-            self.S_NAME = S_NAME;
-            self.PHONE = PHONE;
+            self.mapData = mapData;
         });
+    },
+    computed: {
+        checkUseYN() {
+            if (this.mapData?.use_yn === 'Y') {
+                return '사용 가능';
+            } else if (this.mapData?.use_yn === 'N') {
+                return '사용 불가';
+            } else {
+                return '미확인';
+            }
+        }
+    },
+    methods: {
+        linkNaver() {
+            window.open(`https://map.naver.com/v5/search/${this.mapData.ADDRESS}`, "_blank");
+        },
+        linkKakao() {
+            window.open(`https://map.kakao.com/link/map/${this.mapData.S_NAME},${this.mapData.X_COORDINATE},${this.mapData.Y_COORDINATE}`, "_blank");
+        },
+        report() {
+            reportAddress({ S_CODE: this.mapData.S_CODE })
+                .then(() => {
+                    this.$message({
+                        message: '신고 접수가 완료되었습니다. 감사합니다.',
+                        type: 'success'
+                    });
+                });
+        }
     }
 }
 </script>
 
 <style scoped>
-.address {font-size: 17px;margin-bottom: 30px;}
+.address {
+    font-size: 17px;margin-bottom: 30px;
+}
 .container span:first-child {
     margin-right: 10px;;
     background:#964b4b;
@@ -42,5 +79,17 @@ export default {
     color: #fff;
 }
 
-.mobile >>> .el-dialog {width: 90% !important;}
+.mobile >>> .el-dialog {
+    width: 90% !important;
+}
+.container {
+    display: flex;justify-content: space-between;
+}
+.map-detail i {
+    cursor: pointer;
+}
+.use-yn {
+    text-align: right;
+    margin-bottom: 10px;
+}
 </style>
